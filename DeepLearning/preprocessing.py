6386800d6b4269012ceff9a369fdff070745cb67
gavan <gavan.keane@oracle.com>
2019-06-17 12:41:21 +0100

    change normalisation method to use variance instead of std

--- DeepLearning/preprocessing.py
+++ DeepLearning/preprocessing.py
@@ -16,41 +16,44 @@ def z_scale(data, per_channel=False):
     Returns the calculated means and variance for each channel
 
     data        (numpy array): All data, samples(N) x channels(C) x rows(H) x columns(W)
-    per_channel (boolean):     If True will calculate separate means and std for each image channel
+    per_channel (boolean):     If True will calculate separate means and variance for each image channel
     """
 
     means = list()
-    stds = list()
+    variances = list()
 
     if per_channel:
         for channel in range(data.shape[1]):
             means.append(np.mean(data[:,channel]))
-            stds.append(np.std(data[:,channel]))
+            variances.append(np.var(data[:,channel]))
 
-            data[:,channel] = (data[:,channel] - means[channel]) / stds[channel]
+            data[:,channel] = (data[:,channel] - means[channel]) / variances[channel]
 
     else:
         means.append(np.mean(data))
-        stds.append(np.std(data))
+        variances.append(np.var(data))
 
-        data = (data - means[0]) / stds[0]
+        data = (data - means[0]) / variances[0]
 
-    return data, means, stds
+    return data, means, variances
 
 
 # ===================
 # WEIGHT INITALISTAION FUNCTIONS
 # ===================
 
-def xavier_initalisation(num_inputs, num_outputs):
+def xavier_initalisation(num_inputs, num_outputs, relus=False):
     """ Generates weight matrix using xavier initalisation
     Xavier initalisation aims to create a weights matrix with a variance of 1
+    Since ReLu kills half the actiavtions at each layer we account for this by dividing again by 2
 
-    num_inputs  (int): number of inputs to the layer
-    num_outputs (int): number of outputs from the layer
+    num_inputs  (int):     Number of inputs to the layer
+    num_outputs (int):     Number of outputs from the layer
+    relus:      (boolean): Set to True if using ReLu activation functions
     """
 
-    return np.random.normal(0, 1, size=(num_inputs, num_outputs)) / np.sqrt(num_inputs)
+    relu_offset = 2.0 if relus else 1.0
+    return np.random.normal(0, 1, size=(num_inputs, num_outputs)) / np.sqrt(num_inputs / relu_offset)
 
 
 def kaiming_initalisation():
